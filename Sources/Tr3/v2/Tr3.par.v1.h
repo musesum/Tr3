23d35bf29146759115985fae21e39f71c7e57520
musesum <warren@muse.com>
2020-11-29 17:34:42 -0800

    v2 tags

---
+++ Sources/Tr3/v2/Tr3.par.v1.h
@@ -0,0 +1,62 @@
+// This Tr3.par file renamed to Tr3.par.h to allow xcode to do some syntax highlighting.
+// This file is ignored by the parser. The real source is a string Tr3Par, in Tr3.par.swift.
+
+tr3: left right* {
+
+    left: comment* (path | name | quote)
+
+    right: (tr3Val | child | many | proto | array | edges | embed | comment)+
+
+    child    :  "{" tr3+ "}"
+    many:   ":" "{" tr3+ "}"
+    proto:  ":" (path | name)
+    tr3Val: ":" value
+
+    value:  (scalar | tuple | quote)
+
+    scalar: ("(" scalar1 ")" | scalar1)
+    scalar1: (thru | upto | modu | incr | decr | data | dflt) {
+
+        thru: min "..." max ("=" dflt)?
+        upto: min "..<" max ("=" dflt)?
+        modu: "%" max ("=" dflt)?
+        incr: "++"
+        decr: "--"
+        data: "*"
+        min:  num
+        max:  num
+        dflt: num
+    }
+    tuple: "(" (nameNums | names | nums) ")" tupVal? {
+        names:    name{2,}
+        nums:     num{2,}
+        nameNums: (name ":" num){1,}
+        tupVal:   ":" (scalar1 | tuple)
+    }
+    edges: edgeOp (edgePar | edgeItem) comment* {
+
+        edgeOp:   '^([<][-=?!\╌>]+|[-=?!\˚]+[>])'
+        edgePar:  "(" edgeItem+ ")" edges?
+        edgeItem: (edgeVal | ternary) comment*
+
+        edgeVal:  (path | name) (edges+ | ":" value)?
+
+        ternary:  ("(" tern ")" | tern) {
+
+            tern:        ternIf ternThen ternElse? ternRadio?
+            ternIf:      (path | name) ternCompare?
+            ternThen:    "?" (ternary | path | name | value)
+            ternElse:    ":" (ternary | path | name | value)
+            ternRadio:   "|" ternary
+            ternCompare: compare (path | name | value)
+        }
+    }
+    path:    '^((([A-Za-z_][A-Za-z0-9_]*)*([.˚*])+([A-Za-z_][A-Za-z0-9_.˚*]*)*)+)'
+    name:    '^([A-Za-z_][A-Za-z0-9_]*)'
+    quote:   '^\"([^\"]*)\"'
+    num:     '^([+-]*([0-9]+[.][0-9]+|[.][0-9]+|[0-9]+[.](?![.])|[0-9]+)([e][+-][0-9]+)?)'
+    array:   '^\:?\[[ ]*([0-9]+)[ ]*\]'
+    comment: '^[/][/][ ]*((.*?)[\r\n]+|^[ \r\n\t]+)'
+    compare: '^[<>!=][=]?'
+    embed:   '^[{][{](?s)(.*?)[}][}]'
+}
